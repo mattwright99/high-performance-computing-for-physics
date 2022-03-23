035519ee89362f63e2cd4626f22976c8c6afd044
Matt Wright <16mpw1@queensu.ca>
2022-03-23 02:22:40 -0400

    Part 2 (a-b)

--- ps5/main.py
+++ ps5/main.py
@@ -43,11 +43,11 @@ rcParams['ytick.major.width'] = 1
 time_pause = 0.8  # specifies time a frame is displayed in animation
 save_plot = False  # 0 will not save graphics 
 cycle = 100  # for graph updates
-animate_flag = False  # specify whether or not to animate result
+animate_flag = True  # specify whether or not to animate result
 
 # initialize graph, fixed scaling for this first example
 def init_plt_1():
-    plt.ylim((-0.7, 0.7))
+    plt.ylim((-0.8, 0.8))
     plt.xlim((0, n_xpts-1))    
     plt.axvline(x=n_xpts//2,color='r')  # Vert line separator
     plt.grid('on')
@@ -55,10 +55,10 @@ def init_plt_1():
     ax.set_ylabel('$E_x$')
     plt.pause(1)
 
-def init_plt_1cd(dielectric_start, dielectric_end):
-    plt.ylim((-0.7, 0.7))
+def init_plt_1cd(d_start, dielectric_end):
+    plt.ylim((-0.8, 0.8))
     plt.xlim((0, 600))    
-    plt.axvline(x=dielectric_start,color='r')  # Vert line separator
+    plt.axvline(x=d_start,color='r')  # Vert line separator
     plt.axvline(x=dielectric_end,color='r')  # Vert line separator
     plt.grid('on')
     ax.set_xlabel('Grid Cells ($z$)')
@@ -120,6 +120,53 @@ def plot_1cd(E_in, E_t, E_r, L, epsilon):
     # plt.savefig("./figs/p1d.pdf", dpi=800)
     plt.show()
 
+def plot_2b(E_in, E_t, E_r, L):
+    # Plotting function for part 1 (c) and (d)
+    fig = plt.figure(figsize=(8,6))
+
+    # Plot electric fields over time
+    ax1 = fig.add_axes([.2, .6, .6, .3])
+    ax1.plot(range(n_steps + 1), E_in, 'b-', label=r'$E_{in}$')
+    ax1.plot(range(n_steps + 1), E_t, 'r-', label=r'$E_{t}$')
+    ax1.plot(range(n_steps + 1), E_r, 'g-', label=r'$E_{r}$')
+    ax1.set_xlim(0, 1500)
+    ax1.set_ylabel(r'$E_x$')
+    ax1.set_xlabel(r'Time $(\Delta t)$')
+    ax1.legend(loc='upper right')
+
+    # Plot reflecred and transmitted in frequency domain
+    ax2 = fig.add_axes([.2,.2,.6,.3])
+    # find frequency domain reps of E fields
+    E_in_f = np.fft.rfft(E_in, norm='ortho')
+    E_t_f = np.fft.rfft(E_t, norm='ortho')
+    E_r_f = np.fft.rfft(E_r, norm='ortho')
+    freq = np.fft.rfftfreq(E_in.size, d=dt)
+    # get the analytical solution to compare
+    omega = 2*np.pi * freq
+    epsilon = 1 - omega_p**2 / (omega**2 + 1j * omega * alpha)
+    r, t = get_analytical_soln(L, epsilon, freq)
+    T_an = np.abs(t)**2
+    R_an = np.abs(r)**2
+    # compute transmisison and reflection coefficients vs freq
+    T = np.abs(E_t_f/ E_in_f)**2
+    R = np.abs(E_r_f)**2 / np.abs(E_in_f)**2
+    # plot all pretty
+    freq = freq / tera  # scale to THz
+    ax2.plot(freq, T, 'r', label=r'$T$')
+    ax2.plot(freq, T_an, 'r--', label=r'$T_{an}$')
+    ax2.plot(freq, R, 'b', label=r'$R$')
+    ax2.plot(freq, R_an, 'b--', label=r'$R_{an}$')
+    ax2.plot(freq, T+R,'g' , label='Sum')
+    ax2.set_xlim(0, 300)
+    ax2.set_ylim(-0.05, 1.05)
+    ax2.set_ylabel(r'$T, R$')
+    ax2.set_xlabel(r'$\omega /2\pi$ (THz)')
+    ax2.legend(loc='lower right')
+
+    # plt.savefig("./figs/p2b.pdf", dpi=800)
+    plt.show()
+
+
 def get_analytical_soln(L, eps, f):
     # Analytical solutions for T and R using an assumed harmonic solution
     k0 = 2*np.pi * f / c
@@ -141,19 +188,21 @@ epsilon_0 = constants.epsilon_0
 dx = 20e-9 #  FDTD grid size in space, in SI Units
 dt = dx / (2 * c) # FDTD time step
 
-Ex = np.zeros(n_xpts, dtype=np.float64)  # E array  
-Hy = np.zeros(n_xpts, dtype=np.float64)  # H array
+# Pulse parameters and points per wavelength
+def check_ppw(n, freq_in, dx):
+    # Function checks that there are enough points per wavelength for a given material
+    v = c / n  # speed of EM wave in medium
+    lam =  2*np.pi * v / freq_in  # wavelength
+    ppw = lam // dx  # points per wavelength
+    if ppw <= 15:
+        raise Exception(f'Points per wavelength should be > 15 but got {ppw}')
 
-# Pulse parameters and points per wavelength"
 isource = 200  # source position
 spread = 2 * fs/dt  # 2 fs for this example
 t0 = spread * 6
 freq_in = 2*np.pi * 200 * tera  # incident (angular) frequency
 w_scale = freq_in * dt
-lam = 2*np.pi * c / freq_in  # near 1.5 microns
-ppw = lam // dx  # points per wavelength
-if ppw <= 15:
-    raise Exception(f'Points per wavelength should be > 15 but got {ppw}')
+check_ppw(1, freq_in, dx)
 
 # Pulse function
 pulse_fn = lambda t: -np.exp(-0.5*(t-t0)**2/spread**2)*(np.cos(t*w_scale))
@@ -166,6 +215,9 @@ def a_FDTD_loop_1D(nsteps, cycle):
     # Keep track of previous 2 values of E at boundary for absorbing BC
     Ex_1 = {'n-1' : 0,  'n-2' : 0}
     Ex_end = {'n-1' : 0,  'n-2' : 0}
+
+    Ex = np.zeros(n_xpts, dtype=np.float64)  # E array  
+    Hy = np.zeros(n_xpts, dtype=np.float64)  # H array
     
     # loop over all time steps
     for i in range(nsteps+1):
@@ -201,11 +253,14 @@ def b_FDTD_loop_1D(nsteps, cycle):
     Ex_1 = {'n-1' : 0,  'n-2' : 0}
     Ex_end = {'n-1' : 0,  'n-2' : 0}
     
+    Ex = np.zeros(n_xpts, dtype=np.float64)  # E array  
+    Hy = np.zeros(n_xpts, dtype=np.float64)  # H array
+
     # loop over all time steps
     for i in range(nsteps+1):
         t = i-1  # iterative time dep pulse as source
-        Ex_source = pulse_fn(t)
-        Hy_source = pulse_fn(t + 0.5)
+        E_pulse = pulse_fn(t)
+        H_pulse = pulse_fn(t + 0.5)
 
         # Update stored boundary points
         Ex_1['n-2'] = Ex_1['n-1']
@@ -215,7 +270,7 @@ def b_FDTD_loop_1D(nsteps, cycle):
 
         # Update E
         Ex[1:-1] = Ex[1:-1] + 0.5 * (Hy[0:-2] - Hy[1:-1])
-        Ex[isource] = Ex[isource] - 0.5 * Hy_source
+        Ex[isource] = Ex[isource] - 0.5 * H_pulse
 
         # Apply absorbing BCs
         Ex[0] = Ex_1['n-2']
@@ -223,20 +278,20 @@ def b_FDTD_loop_1D(nsteps, cycle):
                 
         # Update H - note the offset
         Hy[0:-1] = Hy[0:-1] + 0.5 * (Ex[0:-1] - Ex[1:])
-        Hy[isource - 1] = Hy[isource - 1] - 0.5 * Ex_source
+        Hy[isource - 1] = Hy[isource - 1] - 0.5 * E_pulse
 
         # Update graph every cycle
         if animate_flag:
             update_plot(i, cycle, Ex)
 
 def c_FDTD_loop_1D(nsteps, cycle, L, epsilon):
-    thickness_idx = int(L / dx)  # thickness in terms of x indices
-    dielectric_start = 300  # initial position of dielectric
+    d_start = 300  # initial position of dielectric
+    d_thickness = int(L / dx)  # thickness in terms of x indices
     permitivity_coeff = np.ones(n_xpts)
-    permitivity_coeff[dielectric_start : dielectric_start + thickness_idx] = 1 / epsilon
+    permitivity_coeff[d_start : d_start + d_thickness] = 1 / epsilon
 
     if animate_flag:
-        init_plt_1cd(dielectric_start, dielectric_start+thickness_idx)
+        init_plt_1cd(d_start, d_start+d_thickness)
 
     E_in = np.empty(n_steps+1)  # incident field over time
     E_t = np.empty(n_steps+1)  # transmitted field over time
@@ -246,15 +301,18 @@ def c_FDTD_loop_1D(nsteps, cycle, L, epsilon):
     Ex_1 = {'n-1' : 0,  'n-2' : 0}
     Ex_end = {'n-1' : 0,  'n-2' : 0}
 
+    Ex = np.zeros(n_xpts, dtype=np.float64)  # E array  
+    Hy = np.zeros(n_xpts, dtype=np.float64)  # H array
+
     # loop over all time steps
     for i in range(nsteps+1):
         t = i-1  # iterative time dep pulse as source
-        Ex_source = pulse_fn(t)
-        Hy_source = pulse_fn(t + 0.5)
+        E_pulse = pulse_fn(t)
+        H_pulse = pulse_fn(t + 0.5)
 
         E_in[i] = pulse_fn(t)
-        E_t[i] = Ex[dielectric_start + thickness_idx + 50]
-        E_r[i] = Ex[isource - 50]
+        E_t[i] = Ex[d_start + d_thickness + 10]
+        E_r[i] = Ex[isource - 10]
 
         # Update stored boundary points
         Ex_1['n-2'] = Ex_1['n-1']
@@ -264,7 +322,7 @@ def c_FDTD_loop_1D(nsteps, cycle, L, epsilon):
 
         # Update E
         Ex[1:-1] = Ex[1:-1] + 0.5 * permitivity_coeff[1:-1] * (Hy[0:-2] - Hy[1:-1])
-        Ex[isource] = Ex[isource] - 0.5 * Hy_source
+        Ex[isource] = Ex[isource] - 0.5 * H_pulse
 
         # Apply absorbing BCs
         Ex[0] = Ex_1['n-2']
@@ -272,7 +330,7 @@ def c_FDTD_loop_1D(nsteps, cycle, L, epsilon):
 
         # Update H - note the offset
         Hy[0:-1] = Hy[0:-1] + 0.5 * (Ex[0:-1] - Ex[1:])
-        Hy[isource - 1] = Hy[isource - 1] - 0.5 * Ex_source
+        Hy[isource - 1] = Hy[isource - 1] - 0.5 * E_pulse
 
         # Update graph every cycle
         if animate_flag:
@@ -286,17 +344,18 @@ def c_FDTD_loop_1D(nsteps, cycle, L, epsilon):
 if animate_flag:
     fig = plt.figure(figsize=(8,6))
     ax = fig.add_axes([.18, .18, .7, .7])
-    im, = ax.plot(np.arange(1, n_xpts-1), Ex[1:n_xpts-1], linewidth=2)
+    im, = ax.plot(np.arange(1, n_xpts-1), np.zeros(n_xpts-2), linewidth=2)
 
 "Main FDTD: time steps = nsteps, cycle for very simple animation"
 # # initialize, then we will just update the y data and title frame
 # a_FDTD_loop_1D(n_steps, cycle)
 # b_FDTD_loop_1D(n_steps, cycle)
 
-run = True  # whether to run simulation or load saved results
+run = False  # whether to run simulation or load saved results
 n_steps = 30000  # simulate for a long time to ensure signals die down
 epsilon = 9  # permitivity coefficient
 L = 1e-6  # length of dielectric
+check_ppw(np.sqrt(epsilon), freq_in, dx)
 if run:
     E_in, E_t, E_r = c_FDTD_loop_1D(n_steps, cycle, L, epsilon)
     np.save('E_in', E_in)
@@ -307,6 +366,101 @@ else:
     E_t = np.load('E_t.npy')
     E_r = np.load('E_r.npy')
 
-plot_1cd(E_in, E_t, E_r, L, epsilon)
+# plot_1cd(E_in, E_t, E_r, L, epsilon)
 
 
+#%%  PART B
+
+def q2a_FDTD_loop_1D(nsteps, cycle, L, epsilon):
+    d_start = 300  # initial position of dielectric
+    d_thickness = int(L / dx)  # thickness in terms of x indices
+    
+    if animate_flag:
+        init_plt_1cd(d_start, d_start+d_thickness)
+
+    E_in = np.empty(n_steps+1)  # incident field over time
+    E_t = np.empty(n_steps+1)  # transmitted field over time
+    E_r = np.empty(n_steps+1)  # refelcted field over time
+
+    # Keep track of previous 2 values of E at boundary for absorbing BC
+    Dx_1 = {'n-1' : 0,  'n-2' : 0}
+    Dx_end = {'n-1' : 0,  'n-2' : 0}
+
+    Ex = np.zeros(n_xpts, dtype=np.float64)  # E array  
+    Hy = np.zeros(n_xpts, dtype=np.float64)  # H array
+    Dx = np.zeros(n_xpts, dtype=np.float64)  # D array
+    Sx_prev = {
+        'n-1' : np.zeros(d_thickness, dtype=np.float64),
+        'n-2' : np.zeros(d_thickness, dtype=np.float64)
+    }
+    # loop over all time steps
+    for i in range(nsteps+1):
+        t = i-1  # iterative time dep pulse as source
+        E_pulse = pulse_fn(t)
+        H_pulse = pulse_fn(t + 0.5)
+
+        E_in[i] = pulse_fn(t)
+        E_t[i] = Ex[d_start + d_thickness + 10]
+        E_r[i] = Ex[isource - 10]
+
+        # Update stored boundary points
+        Dx_1['n-2'] = Dx_1['n-1']
+        Dx_1['n-1'] = Dx[1]
+        Dx_end['n-2'] = Dx_end['n-1']
+        Dx_end['n-1'] = Dx[-2]
+
+        # Update D
+        Dx[1:-1] = Dx[1:-1] + 0.5 * (Hy[0:-2] - Hy[1:-1])
+        Dx[isource] = Dx[isource] - 0.5 * H_pulse
+
+        # Apply absorbing BCs
+        Dx[0] = Dx_1['n-2']
+        Dx[-1] = Dx_end['n-2']
+
+        # Update E
+        Sx = (1 + np.exp(-alpha * dt)) * Sx_prev['n-1'] \
+            - np.exp(-alpha*dt) * Sx_prev['n-2'] \
+            + dt * omega_p**2 / alpha * (1 - np.exp(-alpha * dt)) * Ex[d_start : d_start + d_thickness]
+        
+        Ex = 1 * Dx  # n=1 outside of the dielecrix
+        Ex[d_start : d_start + d_thickness] = Dx[d_start : d_start + d_thickness] - Sx  # Drude dispersion model
+        # update previous two S values for next iteration
+        Sx_prev['n-2'] = Sx_prev['n-1']
+        Sx_prev['n-1'] = Sx
+
+        # Update H - note the offset
+        Hy[0:-1] = Hy[0:-1] + 0.5 * (Ex[0:-1] - Ex[1:])
+        Hy[isource - 1] = Hy[isource - 1] - 0.5 * E_pulse
+
+        # Update graph every cycle
+        if animate_flag:
+            update_plot(i, cycle, Ex)
+    
+    return E_in, E_t, E_r
+
+
+omega_p = 1.26e15
+alpha = 1.4e14
+
+spread = 1 * fs/dt  # 1 fs for this example
+t0 = spread * 6
+# check_ppw(np.sqrt(epsilon), freq_in, dx)
+pulse_fn = lambda t: -np.exp(-0.5*(t-t0)**2/spread**2)*(np.cos(t*w_scale))  # Pulse function
+
+run = True  # whether to run simulation or load saved results
+n_steps = 30000  # simulate for a long time to ensure signals die down
+L = 500e-9
+if run:
+    if n_steps > 3000:
+        animate_flag = False
+    E_in, E_t, E_r = q2a_FDTD_loop_1D(n_steps, cycle, L, epsilon)
+    np.save('E_in', E_in)
+    np.save('E_t', E_t)
+    np.save('E_r', E_r)
+else:
+    E_in = np.load('E_in.npy')
+    E_t = np.load('E_t.npy')
+    E_r = np.load('E_r.npy')
+
+
+plot_2b(E_in, E_t, E_r, L)
